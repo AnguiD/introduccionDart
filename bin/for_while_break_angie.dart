25ac8b2934a31534099bc3b39a4e1a5a99e1c682
GUERRERO GONZALEZ ANGELICA <145416012+AnguiD@users.noreply.github.com>
2024-11-16 19:26:47 -0600

    Update for_while_break_angie.dart

--- bin/for_while_break_angie.dart
+++ bin/for_while_break_angie.dart
@@ -1,54 +1,53 @@
 void main() {
-  // --- For: NUMERO PAR
-  print('--- For: Buscando un número par ---');
+  // For
+  print('--- Numero Par Ciclo For ---');
   List<int> numeros = [1, 3, 5, 7, 8, 10, 12];
   for (int i = 0; i < numeros.length; i++) {
     print('Revisando número: ${numeros[i]}');
     if (numeros[i] % 2 == 0) {
-      // Buscar el primer número par
       print('¡Número par ${numeros[i]} encontrado! Deteniendo la búsqueda.');
       break;
     }
   }
   print('');
 
-  // --- While:BUSCAR TESORO
-  print('--- While: Buscando un tesoro ---');
-  List<String> ubicaciones = ['Bosque', 'Montaña', 'Valle', 'Río', 'Cueva'];
+  // While
+  print('--- Busqueda alumno con ciclo While ---');
+  List<String> ubicaciones = ['Diego', 'Ros', 'Griss', 'Chucho', 'Angie'];
   int contador = 0;
   bool tesoroEncontrado = false;
   while (contador < ubicaciones.length && !tesoroEncontrado) {
     print('Buscando en ${ubicaciones[contador]}...');
-    if (ubicaciones[contador] == 'Cueva') {
-      print('¡Tesoro encontrado en la Cueva!');
+    if (ubicaciones[contador] == 'Angie') {
+      print('La alumna Angie fue encontrada');
       tesoroEncontrado = true;
     }
     contador++;
   }
   print('');
 
-  // --- Do-While: CLIMA
-  print('--- Do-While: Verificando el clima ---');
-  int temperatura = 10;
+  // Do-While
+  print('--- Temperatura con ciclo Do-While ---');
+  int temperatura = 9;
   do {
     print('Temperatura actual: $temperatura°C');
     temperatura++;
-  } while (temperatura < 15);
+  } while (temperatura < 18);
   print('¡La temperatura es lo suficientemente alta para salir!\n');
 
-  // --- Break: BUSCAR CIUDAD
-  print('--- Break: Buscando una ciudad específica ---');
+  // Break
+  print('--- Buscar ciudad con uso de break ---');
   List<String> ciudades = [
-    'Madrid',
-    'Barcelona',
-    'Valencia',
-    'Sevilla',
-    'Bilbao'
+    'Guadalajara',
+    'CDMX',
+    'Monterrey',
+    'Morelia',
+    'Acapulco'
   ];
   for (String ciudad in ciudades) {
     print('Revisando ciudad: $ciudad');
-    if (ciudad == 'Sevilla') {
-      print('¡Ciudad Sevilla encontrada! Deteniendo la búsqueda.');
+    if (ciudad == 'Morelia') {
+      print('¡Ciudad de Morelia encontrada! Deteniendo la búsqueda.');
       break;
     }
   }
